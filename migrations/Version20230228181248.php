835775c0a99d061a9661001fc14216aca927600e
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-02-28 21:13:46 +0100

    Ajout des avatars des comediens

--- migrations/Version20230228181248.php
+++ migrations/Version20230228181248.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230227195332 extends AbstractMigration
+final class Version20230228181248 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/ComedienFixtures.php
+++ src/DataFixtures/ComedienFixtures.php
@@ -11,14 +11,24 @@ use DateTimeImmutable;
 class ComedienFixtures extends Fixture
 {
 
+
     public function load(ObjectManager $manager):void
     {
         $faker = Factory::create('fr_FR');
 
-        for ($i = 0; $i < 130; $i++ ){
+        $context = stream_context_create([
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false
+            ]
+        ]);
+        for ($i = 0; $i < 49; $i++ ){
 
             $comedien = new Comedien();
-            $prenom = $faker ->firstName;
+            $sexe = $faker->randomElement(['Homme', 'Femme']);
+            $comedien -> setSexe($sexe);
+            $sexe === "Homme" ? $prenom = $faker ->firstNameMale : $prenom = $faker ->firstNameFemale;
+
             $nom = $faker -> lastName;
             $format = '06 ## ## ## ##'; // format du numéro de téléphone
             $numero = $faker->numerify($format);
@@ -26,14 +36,27 @@ class ComedienFixtures extends Fixture
             $comedien -> setPrenom($prenom);
             $comedien -> setNom($nom);
 
-            $sexe = $faker->randomElement(['Homme', 'Femme']);
-            $comedien -> setSexe($sexe);
+
+
             $comedien -> setEmail($prenom.".".$nom."@gmail.com");
             $comedien -> setAge(rand(18,75));
             $comedien -> setTelephone($numero);
             $comedien -> setTauxJournalier(rand(4, 20) * 25);
             $comedien -> setUpdatedAt(new DateTimeImmutable());
-            $comedien -> setImageName('inconnu.jpg');
+
+
+            // Utilisation de la unsplash LoremPixel pour générer des images aléatoires
+            $access_key = 'Nk5l75fEO8Iz2SjGoZfH1Hk7csYI4fCVBYpDwWIesHc';
+
+            $image_url = sprintf('https://api.unsplash.com/photos/random?query=%s&orientation=squarish&client_id=%s', $sexe === 'Homme' ? 'man' : 'woman', $access_key);
+            $image_json = file_get_contents($image_url);
+            $image_data = json_decode($image_json, true);
+            $avatar_url = $image_data['urls']['small'];
+            $avatar_content = file_get_contents($avatar_url);
+            $avatar_name = sprintf('%s_%s_%s.jpg', $prenom, $nom, $sexe);
+            $avatar_dir = dirname(__DIR__,2) . '\public\images\avatar\\';
+            file_put_contents($avatar_dir . $avatar_name, $avatar_content);
+            $comedien->setImageName($avatar_name);
 
             $manager->persist($comedien);
 
