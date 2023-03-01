47c2f4fc734f7b30e1f477b591824970310c6c7f
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-03-01 18:39:09 +0100

    Changement de la route Accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'main')]
+    #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
