4a1968b01a2e20158dbd89bc53638e8dc6248e19
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-03-02 22:50:56 +0100

    controle du calendrier

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,18 @@
-const telephoneInput = document.querySelector('[data-telephone]');
 
-telephoneInput.addEventListener('input', (e) => {
-    const cleanedValue = e.target.value.replace(/\D/g, '').slice(0, 10);
-    const formattedValue = cleanedValue.replace(/(\d{2})(?=\d)/g, '$1 ');
-    e.target.value = formattedValue;
+
+//Pour les calendrier
+$(document).ready(function() {
+    // you may need to change this code if you are not using Bootstrap Datepicker
+    $('.js-datepicker').datepicker({
+        dateFormat: 'dd/mm/yy', // format de date
+        minDate: 0 // empêche de sélectionner des dates antérieures à aujourd'hui
+    });
 });
+
+// const telephoneInput = document.querySelector('[data-telephone]');
+//
+// telephoneInput.addEventListener('input', (e) => {
+//     const cleanedValue = e.target.value.replace(/\D/g, '').slice(0, 10);
+//     const formattedValue = cleanedValue.replace(/(\d{2})(?=\d)/g, '$1 ');
+//     e.target.value = formattedValue;
+// });
